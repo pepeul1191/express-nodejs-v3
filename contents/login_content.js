09525df45b42add2990428a44b19a88496bcd03c
Pepe Valdivia <jvaldivia@softweb.pe>
2018-09-12 17:46:08 -0500

    login contra servicio

--- contents/login_content.js
+++ contents/login_content.js
@@ -28,5 +28,29 @@ exports.content = {
       es: 'Error en solicitud Token CSRF',
       en: 'Token CSRF request error',
     },
+    message_user_system: {
+      es: 'Usuario no se encuentra registrado en el sistema',
+      en: 'User is not registered in the system',
+    },
+    message_user_system_error: {
+      es: 'Se ha producido un error no esperado al validar usuario/sistema',
+      en: 'An unexpected error occurred when validating user/system',
+    },
+    message_user_system_catch_error: {
+      es: 'No se puede acceder al servicio de validación de usuario/sistema',
+      en: 'The user/system validation service can not be accessed',
+    },
+    message_user_password: {
+      es: 'Usuario y/o contraseña no coincide',
+      en: 'User and/or password does not match',
+    },
+    message_user_password_error: {
+      es: 'Se ha producido un error no esperado al validar usuario/contraseña',
+      en: 'An unexpected error occurred when validating user/password',
+    },
+    message_user_password_catch_error: {
+      es: 'No se puede acceder al servicio de validación de usuario/contraseña',
+      en: 'The user/password validation service can not be accessed',
+    },
   },
 };
--- routes/login.js
+++ routes/login.js
@@ -1,11 +1,11 @@
 var express = require('express');
+var request = require('sync-request');
 var router = express.Router();
 var constants = require('../configs/constants');
 var helpers = require('../configs/helpers');
 var loginHelper = require('../helpers/login_helper');
 var loginContent = require('../contents/login_content');
 var middlewares = require('../configs/middlewares');
-var request = require('sync-request');
 
 router.get('/', middlewares.sessionFalse(), function(req, res, next) {
   var locals = {
@@ -14,7 +14,7 @@ router.get('/', middlewares.sessionFalse(), function(req, res, next) {
     helpers: helpers,
     csss: loginHelper.indexCss(),
     jss: loginHelper.indexJs(),
-    mensaje: '',
+    message: '',
     contents: loginContent.content,
     lang: middlewares.lang(req),
   };
@@ -27,30 +27,92 @@ router.post('/access', function(req, res, next) {
   var password = req.body.password;
   var body = '';
   var lang = middlewares.lang(req);
-  if(req_csrf_val != constants.data.csrf.secret){
+  var message = '';
+  //validation CSRF
+  if(req_csrf_val == constants.data.csrf.secret){
+    //validation user/system
+    var _continue = true;
+    try{
+      var r1 = request('POST', constants.data.accesos.url + 'sistema/usuario/validar', {
+        headers: {
+          [constants.data.accesos.csrf_key]: constants.data.accesos.csrf_value,
+          'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        body: 'usuario=' + user + '&sistema_id=' + constants.data.sistema_id,
+      });
+      if (r1.statusCode == 200){
+        if(r1.body.toString('utf8') != '1'){
+          _continue = false;
+          message = loginContent.content.index.message_user_system[lang];
+        }
+      }else{
+        _continue = false;
+        message = loginContent.content.index.message_user_system_error[lang];
+      }
+    }catch(err){
+      console.log("**** INICIO - ERROR EN LOGIN (Validación usuario/sistema)***");
+      console.log(err.toString('utf-8'));
+      console.log("**** FIN ***");
+      status = 500;
+      _continue = false;
+      message = loginContent.content.index.message_user_system_catch_error[lang];
+    }
+    //validation user/password
+    if(_continue == true){
+      try{
+        var r1 = request('POST', constants.data.accesos.url + 'usuario/externo/validar', {
+          headers: {
+            [constants.data.accesos.csrf_key]: constants.data.accesos.csrf_value,
+            'Content-Type': 'application/x-www-form-urlencoded',
+          },
+          body: 'usuario=' + user + '&contrasenia=' + password,
+        });
+        if (r1.statusCode == 200){
+          if(r1.body.toString('utf8') != '1'){
+            _continue = false;
+            message = loginContent.content.index.message_user_password[lang];
+          }else{
+            //req.session.time = new Date().toLocaleTimeString();
+            //req.session.user = user;
+            //req.session.state = 'activate';
+            res.redirect('/accesos/');
+          }
+        }else{
+          _continue = false;
+          message = loginContent.content.index.message_user_password_error[lang];
+        }
+      }catch(err){
+        console.log("**** INICIO - ERROR EN LOGIN (Validación usuario/contrasenia)***");
+        console.log(err.toString('utf-8'));
+        console.log("**** FIN ***");
+        status = 500;
+        _continue = false;
+        message = loginContent.content.index.message_user_password_catch_error[lang];
+      }
+    }
+  }else{
     console.log("**** INICIO - ERROR EN LOGIN (CSRF token)***");
     console.log('Error en Token CSRF POST request');
     console.log(body);
     console.log("**** FIN ***");
-    var locals = {
-      constants: constants.data,
-      title: 'Bienvenido',
-      helpers: helpers,
-      csss: loginHelper.indexCss(),
-      jss: loginHelper.indexJs(),
-      constants: constants.data,
-      title: 'Bienvenido',
-      helpers: helpers,
-      csss: loginHelper.indexCss(),
-      jss: loginHelper.indexJs(),
-      mensaje: loginContent.content.index.message_csrf[lang],
-      contents: loginContent.content,
-      lang: lang,
-    };
-    res.render('login/index', locals);
-  }else{
-
+    message = loginContent.content.index.message_csrf[lang];
   }
+  var locals = {
+    constants: constants.data,
+    title: 'Bienvenido',
+    helpers: helpers,
+    csss: loginHelper.indexCss(),
+    jss: loginHelper.indexJs(),
+    constants: constants.data,
+    title: 'Bienvenido',
+    helpers: helpers,
+    csss: loginHelper.indexCss(),
+    jss: loginHelper.indexJs(),
+    message: message,
+    contents: loginContent.content,
+    lang: lang,
+  };
+  res.render('login/index', locals);
 });
 
 module.exports = router;
